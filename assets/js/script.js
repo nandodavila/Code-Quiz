e45a11740c0da0b14c6552f6931d43ec3feec3e1
Nando <nandodavila94@gmail.com>
2021-09-16 20:11:33 -0400

    Made HTML page, and started on js, adding variables needed

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var questionsTxt = document.querySelector(".questions");
+var startBtn = document.querySelector(".start-button");
+var answerChoices = document.querySelector(".random-answers");
+
+var questions = ["What is an array??", "Which is a semantic HTML element?", "Where should you link a stylesheet?",
+"What is a 'Boolean'?", "What is a string??"]
+
+var wrongAnswers1 = ["A type of ray gun", "a true or false statement", "a bit of text"]
+var wrongAnswers2 = ["div", "meta", "var"]
+var wrongAnswers3 = ["The footer", "The main tag", "The body tag"]
+var wrongAnswers4 = ["A bunch of numbers", "A bunch of letters", "an ordered list to store values"]
+var wrongAnswers5 = ["A true or false statement", "Numbers", "Everything in the body tag"]
+var rightAnswers = ["An ordered list of values", "Header", "Head tag", "A value thats true or false", 
+"A squence of characters in a var"]
+
