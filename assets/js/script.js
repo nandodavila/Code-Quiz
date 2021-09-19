e4cd3781d1f97bf1fe8c75847fcfed5c645be777
Nando <nandodavila94@gmail.com>
2021-09-19 18:40:03 -0400

    everything working properly, just trying to get hover to work on answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//document elements
 var questionsTxt = document.querySelector(".questions");
 var startBtn = document.querySelector(".start-button");
 var nxtBtn = document.querySelector(".next-button");
@@ -6,141 +7,148 @@ var timerElement = document.querySelector(".timer-count");
 var highscore = document.querySelector(".highscore");
 var form = document.querySelector(".highscore-form");
 var formInput = document.querySelector("#initials")
-var submitForm =document.querySelector(".submit")
-
-
-
+var submitForm = document.querySelector(".submit")
+//global variable
 var timer;
 var timerCount;
 var answeredQuestions = [];
 var displayedHighscores = [];
 
+//array of question objects
 var questions = [
   {
     question: "What is an array??",
     correctAnswer: "An ordered list of values",
-    choices: ["A type of ray gun", "a true or false statement", "a bit of text", "An ordered list of values" ],
+    choices: ["A type of ray gun", "a true or false statement", "a bit of text", "An ordered list of values"],
   },
   {
     question: "Which is a semantic HTML element?",
     correctAnswer: "<header>",
-    choices: ["<div>", "<header>", "<meta>", "<var>", ],
+    choices: ["<div>", "<header>", "<meta>", "<var>",],
   },
   {
     question: "Where should you link a stylesheet?",
     correctAnswer: "Head tag",
-    choices: ["The footer", "Head tag", "The main tag", "The body tag", ],
+    choices: ["The footer", "Head tag", "The main tag", "The body tag",],
   },
   {
     question: "What is a 'Boolean'?",
     correctAnswer: "A value thats true or false",
-    choices: ["A value thats true or false", "A bunch of numbers", "A bunch of letters", "an ordered list to store values",  ],
+    choices: ["A value thats true or false", "A bunch of numbers", "A bunch of letters", "an ordered list to store values",],
   },
   {
     question: "What is a string??",
     correctAnswer: "A squence of characters in a var",
-    choices: ["A true or false statement", "Numbers", "A squence of characters in a var", "Everything in the body tag",  ],
+    choices: ["A true or false statement", "Numbers", "A squence of characters in a var", "Everything in the body tag",],
   }
 ]
-
+// gets high score from local storage
 function getHighscore() {
+  // gets recent high score value out of local storeage
   var storedHighscore = localStorage.getItem("recentHighscore");
+  // checking if value exsist
   if (storedHighscore) {
+    // parse local storeage value into global variable for later use
     displayedHighscores = JSON.parse(storedHighscore);
   }
 }
-
-function setHighscore () {
-
+//sets highscore to local storage
+function setHighscore() {
   localStorage.setItem("recentHighscore", JSON.stringify(displayedHighscores));
 }
-
+//displays highscore
 function displayHighscores() {
+  //clear pages highscore before then displaying local storage
   var clearDisplay = document.querySelectorAll(".list-score");
+  
   for (i = 0; i < clearDisplay.length; i++) {
     clearDisplay[i].remove();
   }
-  
+// runs for loop so every highscore is shown in local storage
   if (displayedHighscores.length > 0) {
     for (i = 0; i < displayedHighscores.length; i++) {
       var listScore = document.createElement("li");
       listScore.textContent = displayedHighscores[i].initials + ' ' + displayedHighscores[i].score;
       listScore.className = "list-score";
-      document.body.appendChild(listScore);
+      highscore.appendChild(listScore);
     }
   }
 }
 
+//calls functions to run get highscore and display high score
 getHighscore();
 displayHighscores();
 
 //timer count
 function startTimer() {
   timerCount = 50;
-    timer = setInterval(function() {
-      
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        if (timerCount > 0) {
-          
-        }
+  timer = setInterval(function () {
+
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount >= 0) {
+      if (timerCount > 0) {
+
       }
-      if (timerCount <= 0) {
-        clearInterval(timer);
-        loseGame();
-      }
-    }, 1000);
+    }
+    if (timerCount <= 0) {
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
 }
 // checking if the answer is correct when next is pushed
 function checkQuestion() {
-  
+
   if (nxtBtn.dataset.isAnswerCorrect === "false") {
     console.log("question wrong");
-    timerCount = timerCount -10;
-
+    timerCount = timerCount - 10;
   }
-
 }
+
 //start button event 
-startBtn.addEventListener("click", function() {
+startBtn.addEventListener("click", function () {
   startTimer();
   startBtn.setAttribute("style", "display: none")
   displayQuestions();
-
 });
+
 // next button event
-nxtBtn.addEventListener("click", function(event) {
+nxtBtn.addEventListener("click", function (event) {
   checkQuestion();
-  displayQuestions(); 
+  displayQuestions();
   console.log(event)
 });
+
 // displays questions in a random order checking if questions have already been asked
-function displayQuestions () {
-  
+function displayQuestions() {
   console.log(answeredQuestions.length)
   console.log(questions.length)
+  // checks if all the questions have been asked and timer is above zero.
   if (answeredQuestions.length === questions.length && timerCount > 0) {
     stopTimer();
     alert("Quiz over, log your highscore!")
     form.hidden = false;
+    startBtn.setAttribute("style", "display: inline")
+    
     return;
   }
   var questionAnswered = true
-  
-  while(questionAnswered) {
+  //choses random question and checks in the question has already been asked
+  while (questionAnswered) {
     randomQuestion = questions[Math.floor(Math.random() * questions.length)];
-    if (answeredQuestions[randomQuestion.question]) {
-      questionAnswered = true 
+    if (answeredQuestions.includes(randomQuestion.question)) {
+      questionAnswered = true
     } else questionAnswered = false;
   }
-
+  //shows random question
   questionsTxt.textContent = randomQuestion.question
+  //pushes the question asked to the answeredQuestion global variable
   answeredQuestions.push(randomQuestion.question)
 
   for (i = 0; i < answerChoices.length; i++) {
     answerChoices[i].textContent = randomQuestion.choices[i];
-    if (randomQuestion.choices[i] == randomQuestion.correctAnswer){
+    if (randomQuestion.choices[i] == randomQuestion.correctAnswer) {
       answerChoices[i].dataset.isAnswer = true;
     } else answerChoices[i].dataset.isAnswer = false;
   }
@@ -148,8 +156,8 @@ function displayQuestions () {
 
 // adding event listener for all list items
 for (i = 0; i < answerChoices.length; i++) {
-  answerChoices[i].addEventListener("click", function(event) {
-  console.log(event.target.textContent);
+  answerChoices[i].addEventListener("click", function (event) {
+    console.log(event.target.textContent);
     console.log(event.target.dataset);
     if (event.target.dataset.isAnswer === "true") {
       nxtBtn.dataset.isAnswerCorrect = true;
@@ -159,10 +167,10 @@ for (i = 0; i < answerChoices.length; i++) {
       nxtBtn.dataset.isAnswerCorrect = false;
       console.log("Wrong Answer");
     }
-    
+
     nxtBtn.hidden = false;
     console.log(answeredQuestions)
-  }); 
+  });
 }
 
 function loseGame() {
@@ -180,17 +188,13 @@ function getInitials() {
   displayedHighscores.push(newHighscore);
   setHighscore();
   displayHighscores();
-  }
+}
 
-
-
-submitForm.addEventListener('submit', function(event) {
-event.preventDefault();
-getInitials();
+submitForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  getInitials();
 })
 
 function stopTimer() {
   clearInterval(timer);
-
 }
-
